d0a625f378641e5ee7fde4eb9e277963ef62fc19
Nisarg777 <nisargpipaliya2402@gmail.com>
2023-02-06 08:42:30 +0530

    commiting leetcode daily 1470

---
+++ 1470_shuffel_the_array.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+/*
+1470. Shuffle the Array
+Easy
+3.5K
+224
+Companies
+Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+
+Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+
+ 
+
+Example 1:
+
+Input: nums = [2,5,1,3,4,7], n = 3
+Output: [2,3,5,4,1,7] 
+Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7].
+
+
+*/
+
+
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        if(n<=1)
+        {
+            return nums;
+        }
+        int i=1,j=n;
+        while(i<j)
+        {
+            nums.insert(nums.begin()+i,nums[j]);
+            j++;
+            nums.erase(nums.begin()+j);
+            i+=2;
+        }
+        return nums;
+    }
+};
+
+
+/*
+                                                OR
+*/
+
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> ans;
+        for(int i=0,j=n;i<n;i++,j++)
+        {
+            ans.push_back(nums[i]);
+            ans.push_back(nums[j]);
+        }
+        return ans;
+    }
+};
