4b61704d9d1e971c55c94c78fd57072411165e45
Nisarg777 <nisargpipaliya2402@gmail.com>
2023-04-17 08:35:39 +0530

    LC daily

---
+++ 1431_kids_with_greatest_nuber_of_candies.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+1431. Kids With the Greatest Number of Candies
+Easy
+2.5K
+341
+Companies
+There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
+
+Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
+
+Note that multiple kids can have the greatest number of candies.
+
+ 
+
+Example 1:
+
+Input: candies = [2,3,5,1,3], extraCandies = 3
+Output: [true,true,true,false,true] 
+Explanation: If you give all extraCandies to:
+- Kid 1, they will have 2 + 3 = 5 candies, which is the greatest among the kids.
+- Kid 2, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+- Kid 3, they will have 5 + 3 = 8 candies, which is the greatest among the kids.
+- Kid 4, they will have 1 + 3 = 4 candies, which is not the greatest among the kids.
+- Kid 5, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+
+
+
+Can we do it in just one traversal????
+i.e. without finding max element?
+*/
+
+
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int maxi=*max_element(candies.begin(),candies.end()), n = candies.size();
+        vector<bool> ans(n);
+        for(int i = 0; i < n; i++)
+        {
+            ans[i]=(candies[i]+extraCandies >= maxi);
+        }
+        return ans;
+    }
+};
